149b59750bd71fb7bb81d26f044ba4c9c3e2fe09
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2018-12-13 23:24:30 +0500

    Solved Dreamoon and stairs problem

---
+++ DreamonAndStairs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DreamonAndStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            int countTwo = n/2;
+            int countOne = n%2;
+
+            if ( (countOne + countTwo) % m == 0){
+                Console.WriteLine(countOne+countTwo);
+            }else{
+                bool found = false;
+                while (countTwo>0)
+                {
+                    countTwo--;
+                    countOne += 2;
+                    if ((countOne + countTwo) % m == 0) {
+                        found = true;
+                        break;
+                    }
+                }
+                Console.WriteLine((found) ? countOne+countTwo : -1);
+            }
+       }
+    }
+}
