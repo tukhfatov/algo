41b303e9d2ea26adaeece4ea1f1affa7bea40ded
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2018-12-16 00:38:14 +0500

    Solved 25A - IQ Test

---
+++ IqTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string [] input = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.None);
+
+            int countOdd = 0, countEven = 0, oddIndex = 0, evenIndex = 0;
+            for (int i =0; i<n; i++){
+                int k = int.Parse(input[i]);
+                if (k%2 == 0){
+                    countEven++;
+                    if (countEven == 1){
+                        evenIndex = i+1;
+                    }else{
+                        evenIndex = 0;
+                    }
+                }else{
+                    countOdd++;
+                    if (countOdd == 1){
+                        oddIndex = i+1;
+                    }else{
+                        oddIndex = 0;
+                    }
+                }
+            }
+            Console.WriteLine((oddIndex > 0)?oddIndex:evenIndex);
+        }
+    }
+}
