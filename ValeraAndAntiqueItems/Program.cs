7979cce6dc758ecc48519ae7fc28de3b6cbd71bf
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2018-12-19 22:06:13 +0500

    Solved 441A-Valer and Antique Items

---
+++ ValeraAndAntiqueItems/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValeraAndAntiqueItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+
+            int n = int.Parse(input[0]);
+            long v = long.Parse(input[1]);
+
+            List<int> p = new List<int>();
+            for(int i=0; i<n; i++){
+                input = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+                for (int j=1; j<input.Length; j++){
+                    if (long.Parse(input[j]) < v){
+                        p.Add(i+1);
+                        break;
+                    }
+                }
+            }
+            p.Sort();
+            
+            Console.WriteLine(p.Count);
+            for(int i=0; i<p.Count; i++){
+                Console.Write(p[i]+" ");
+            }
+
+        }
+    }
+}
