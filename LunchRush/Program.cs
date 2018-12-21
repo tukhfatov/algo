f081c9e2161735ed36f27a9e2ba7ff72f2f2e91f
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2018-12-21 23:17:31 +0500

    Solved 276A - Lunch Rush

---
+++ LunchRush/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LunchRush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+
+            int n = int.Parse(input[0]);
+            long k = long.Parse(input[1]);
+
+            long result = long.MinValue;
+            for (int i=0; i<n; i++){
+                input = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+                long f = long.Parse(input[0]);
+                long t = long.Parse(input[1]);
+
+                result = Math.Max(result, (t>k)?f-(t-k): f);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
