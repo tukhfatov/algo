442c1317715e02e9b23c7b8e5dc0efd1e494f8cf
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2018-12-21 22:28:19 +0500

    Solved 385A - Bear and Raspberry

---
+++ BearAndRaspberry/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BearAndRaspberry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+
+            int n = int.Parse(input[0]);
+            int c = int.Parse(input[1]);
+
+            input = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.None);
+            int [] x = new int[n];
+
+            for (int i=0; i<n;i++){
+                x[i] = int.Parse(input[i]);
+            }            
+
+            int result = 0;
+            for (int i=0; i<n-1;i++){
+                result = Math.Max(result, x[i] - c - x[i+1]);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
