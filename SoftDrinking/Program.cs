38b9df6ec11ecbf8d3b59880f80b88b13d8cf3ec
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2018-12-25 00:00:02 +0500

    Solved 151A - Soft Drinking

---
+++ SoftDrinking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SoftDrinking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+
+            int n = int.Parse(input[0]);
+            int k = int.Parse(input[1]);
+            int l = int.Parse(input[2]);
+            int c = int.Parse(input[3]);
+            int d = int.Parse(input[4]);
+            int p = int.Parse(input[5]);
+            int nl = int.Parse(input[6]);
+            int np = int.Parse(input[7]);
+
+            Console.WriteLine( Math.Min(Math.Min((k*l)/nl, c*d), p/np) / n);
+        }
+    }
+}
