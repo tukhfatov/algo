359ced71d22c264a40e812d087b39610d31623a4
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2018-12-30 00:41:43 +0500

    Solved 233A - Perfect Permutation

---
+++ A2 Online Judge/Codeforces Div. 2, A/PerfectPermutation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PerfectPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int [] p = new int[n];
+
+            for(int i=0; i<n; i++){
+                p[i] = i+1;
+            }
+
+            for(int i=0; i<n-1; i+=2){
+                int tmp = p[i];
+                p[i] = p[i+1];
+                p[i+1] = tmp;
+            }
+
+            for(int i=0; i<n; i++){
+                if(p[i] == (i+1)){
+                    Console.WriteLine(-1);
+                    return;
+                }
+            }
+            
+            for(int i=0; i<n; i++){
+                Console.Write(p[i]+" ");
+            }
+
+        
+        }
+    }
+}
