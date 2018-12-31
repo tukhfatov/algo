4873a17aee8b013bdcdecd7804f3c29af1d6195c
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2018-12-31 20:28:27 +0500

    Solved 262A - Roma and Lucky Numbers

---
+++ A2 Online Judge/Codeforces Div. 2, A/RomaAndLuckyNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RomaAndLuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+            int n = int.Parse(input[0]);
+            int k = int.Parse(input[1]);
+
+            string [] a = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.None);
+
+            int result = 0;
+            for(int i=0; i<n; i++){
+                int count = 0;
+                string tmp = a[i];
+
+                for(int j=0; j<tmp.Length; j++){
+                    if (tmp[j] == '4' || tmp[j] == '7'){
+                        count++;
+                    }
+                }
+                if (count <= k){
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
