96b3d5951824b993470c37b77a828988790b7321
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2019-01-08 21:57:17 +0500

    Solved 265A - Colorful Stones (Simplified version)

---
+++ A2 Online Judge/Codeforces Div. 2, A/ColorfulStones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ColorfulStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+
+            int cursor = 0;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (s[cursor] == t[i]){
+                    cursor++;
+                }
+            }
+            Console.WriteLine(cursor+1);
+        }
+    }
+}
