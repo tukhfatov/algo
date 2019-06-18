2f3403d385b1191de0cec2ca4a5c5b1b68e2bae1
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2019-06-18 22:27:16 +0500

    Solved 114A - Cifero

---
+++ A2OnlineJudge/Codeforces Div. 2, A/Cifera/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cifera {
+    class Program {
+        static void Main (string[] args) {
+            int k = int.Parse (Console.ReadLine ());
+            int l = int.Parse (Console.ReadLine ());
+
+            int count = 0;
+            while (l % k == 0 && l != 1) {
+                l = l / k;
+                count++;
+            }
+
+            if (l == 1) {
+                Console.WriteLine ("YES");
+                Console.WriteLine (count - 1);
+            } else {
+                Console.WriteLine ("NO");
+            }
+        }
+    }
+}
