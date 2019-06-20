92b2f583b77bb70b79b5795f303dda145d54bab9
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2019-06-21 00:51:38 +0500

    Solved Fibonnaci Number

---
+++ Leetcode/FibonacciNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonacciNumber {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine (Fib (1));
+        }
+        public static int Fib (int N) {
+            var i = 1;
+            var a = 0;
+            var b = 1;
+            if (N == 1) {
+                return b;
+            }
+            var result = 0;
+            while (i < N) {
+                result = a + b;
+                a = b;
+                b = result;
+                i++;
+            }
+            return result;
+        }
+    }
+}
