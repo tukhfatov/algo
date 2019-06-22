20b3eb771940ca830f13f24b9ed1258d70407e06
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2019-06-23 00:01:53 +0500

    Solved Move Zeroes

---
+++ Leetcode/MoveZeroes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoveZeroes {
+    class Program {
+        static void Main (string[] args) {
+            int[] nums = new int[] { 0, 1, 0, 3, 12 };
+            MoveZeroes (nums);
+        }
+        public static void MoveZeroes (int[] nums) {
+            var i = 0;
+            var z = 0;
+            Console.WriteLine (string.Join (" ", nums));
+            while (i < nums.Length) {
+                var cur = nums[i];
+                if (cur == 0) {
+                    z++;
+                }
+                if (cur != 0 && z > 0) {
+                    nums[i - z] = cur;
+                    nums[i] = 0;
+                }
+                i++;
+            }
+            Console.WriteLine (string.Join (" ", nums));
+        }
+    }
+}
