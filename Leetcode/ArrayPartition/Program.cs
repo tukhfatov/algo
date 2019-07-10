714396858cb31a5772a7423388956d85b1c6138e
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2019-07-11 00:41:47 +0500

    Solved Array partition

---
+++ Leetcode/ArrayPartition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayPartition {
+    class Program {
+        static void Main (string[] args) {
+            int[] nums = new int[] { 1, 4, 3, 2 };
+            var result = ArrayPairSum (nums);
+            Console.WriteLine (result);
+        }
+        public static int ArrayPairSum (int[] nums) {
+            Array.Sort (nums);
+            int result = 0;
+            for (int i = 0; i < nums.Length; i += 2) {
+                result += nums[i];
+            }
+            return result;
+        }
+    }
+}
