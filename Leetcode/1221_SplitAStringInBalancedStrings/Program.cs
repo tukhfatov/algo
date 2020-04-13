43c6ada547660276f7c72399298a3b97f5fac14d
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2020-04-13 10:07:25 +0500

    Solved 1221 Split in Balanced strings

---
+++ Leetcode/1221_SplitAStringInBalancedStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1221_SplitAStringInBalancedStrings {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+
+            string s = "RLRRLLRLRL";
+            Console.WriteLine (BalancedStringSplit (s));
+
+            s = "RLRRLLRLRL";
+            Console.WriteLine (BalancedStringSplit (s));
+
+        }
+
+        public static int BalancedStringSplit (string s) {
+            int c = 0;
+            int cc = 0;
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i] == 'R') {
+                    c++;
+                }
+                if (s[i] == 'L') {
+                    c--;
+                }
+
+                if (c == 0) {
+                    cc++;
+                }
+            }
+
+            return cc;
+        }
+    }
+}
