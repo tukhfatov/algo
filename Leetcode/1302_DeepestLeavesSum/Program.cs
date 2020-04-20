f44713e15090c81d1c22a61ec20dcc4f48a97fe8
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2020-04-21 01:34:40 +0500

    Solved 1302 Deepest leaves sum

---
+++ Leetcode/1302_DeepestLeavesSum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _1302_DeepestLeavesSum {
+
+    class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode (int x) { val = x; }
+    }
+
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+        }
+
+        public static int MaxHeight (TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            return Math.Max (MaxHeight (root.left), MaxHeight (root.right)) + 1;
+        }
+
+        public static int S (TreeNode root, int h, int mh) {
+            if (root == null) {
+                return 0;
+            }
+
+            if (h == mh) {
+                return root.val;
+            }
+
+            int l = S (root.left, h + 1, mh);
+            int r = S (root.right, h + 1, mh);
+
+            return l + r;
+        }
+
+        public static int DeepestLeavesSum (TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int maxHeight = MaxHeight (root);
+            Console.WriteLine (maxHeight);
+            int sum = S (root, 1, maxHeight);
+            Console.WriteLine (sum);
+            return sum;
+        }
+    }
+}
