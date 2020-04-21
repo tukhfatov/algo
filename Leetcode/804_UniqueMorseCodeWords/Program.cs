7b6394f10b56ff45a009171e4cb9ecc191f07b02
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2020-04-22 01:52:16 +0500

    Solved 804 - Unique morse code words

---
+++ Leetcode/804_UniqueMorseCodeWords/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _804_UniqueMorseCodeWords {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+        }
+
+        public static int UniqueMorseRepresentations (string[] words) {
+            string[] morse = new string[] {
+                ".-",
+                "-...",
+                "-.-.",
+                "-..",
+                ".",
+                "..-.",
+                "--.",
+                "....",
+                "..",
+                ".---",
+                "-.-",
+                ".-..",
+                "--",
+                "-.",
+                "---",
+                ".--.",
+                "--.-",
+                ".-.",
+                "...",
+                "-",
+                "..-",
+                "...-",
+                ".--",
+                "-..-",
+                "-.--",
+                "--.."
+            };
+
+            HashSet<string> s = new HashSet<string> ();
+
+            foreach (string word in words) {
+                StringBuilder t = new StringBuilder ();
+                foreach (char sym in word) {
+                    t.Append (morse[sym - 'a']);
+                }
+
+                s.Add (t.ToString ());
+            }
+
+            return s.Count;
+        }
+    }
+}
