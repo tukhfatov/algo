bc7db271f7707771504d8c63ec3ea387c29e7487
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2020-04-22 01:32:45 +0500

    Solved 1021 - Remove outermost parentheses

---
+++ Leetcode/1021_RemoveOutermostParentheses/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1021_RemoveOutermostParentheses {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+        }
+
+        public static string RemoveOuterParentheses (string S) {
+            bool[] f = new bool[S.Length];
+            Stack<char> st = new Stack<char> ();
+
+            for (int i = 0; i < S.Length; i++) {
+                if (S[i] == '(') {
+                    st.Push ('(');
+                }
+                f[i] = (st.Count == 1);
+                if (S[i] == ')') {
+                    st.Pop ();
+                }
+            }
+            StringBuilder res = new StringBuilder ();
+            for (int i = 0; i < S.Length; i++) {
+                if (!f[i]) {
+                    res.Append (S[i]);
+                }
+            }
+
+            return res.ToString ();
+
+        }
+    }
+}
