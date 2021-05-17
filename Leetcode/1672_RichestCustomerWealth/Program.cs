8bb6cf26f62180aeb19e2ca043a14bf72b44974d
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-05-17 23:51:21 +0500

    Solved 1672 - Richest Customer Wealth

---
+++ Leetcode/1672_RichestCustomerWealth/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _1672_RichestCustomerWealth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaximumWealth(new int[][] { new int[] { 2, 8, 7 }, new int[] { 7, 1, 3 }, new int[] { 1, 9, 5 } }));
+        }
+
+        public static int MaximumWealth(int[][] accounts)
+        {
+            int result = 0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                result = Math.Max(result, accounts[i].Sum());
+            }
+
+            return result;
+        }
+    }
+}
