bf130858621f2676caa6418ff2aa1944ba5698f6
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-05-18 00:07:14 +0500

    Solved 1470 - Shuffle the Array

---
+++ Leetcode/1470_ShuffleTheArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1470_ShuffleTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", Shuffle(new int[] { 2, 5, 1, 3, 4, 7 }, 3)));
+        }
+
+        public static int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[2 * n];
+
+            int i = 0;
+            int j = 0;
+            while (i < n)
+            {
+                result[j] = nums[i];
+                result[j + 1] = nums[i + n];
+
+                j += 2;
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
