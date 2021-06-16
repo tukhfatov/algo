bf9a92bd089b3d4795dd3823154010d9d50fa78e
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-17 02:45:50 +0500

    Solved 1773 Count items matching a rule

---
+++ Leetcode/1773_CountItemsMatchingARule/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1773_CountItemsMatchingARule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountMatches(
+                new List<IList<string>> {
+                    new List<string> { "phone", "blue", "pixel" },
+                    new List<string> { "computer", "silver", "lenovo" },
+                    new List<string> {"phone","gold","iphone"}
+                },
+                "type",
+                "phone"));
+        }
+
+        public static int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            int res = 0;
+            int col = 0;
+            switch (ruleKey)
+            {
+                case "type":
+                    col = 0;
+                    break;
+                case "color":
+                    col = 1;
+                    break;
+                case "name":
+                    col = 2;
+                    break;
+            }
+
+            foreach (var i in items)
+            {
+                if (i[col] == ruleValue)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
