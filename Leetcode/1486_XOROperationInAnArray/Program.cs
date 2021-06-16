ba60bcf192aeeef877ba06e5400f3e8b803d1e68
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-17 02:59:55 +0500

    Solved 1486 XOR Operation in an Array

---
+++ Leetcode/1486_XOROperationInAnArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1486_XOROperationInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(XorOperation(4, 3));
+        }
+
+        public static int XorOperation(int n, int start)
+        {
+            int res = start;
+            for (int i = 1; i < n; i++)
+            {
+                res = res ^ (start + 2 * i);
+            }
+            return res;
+        }
+    }
+}
