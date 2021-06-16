2ded5895c512200af26aab5af6260cbc562c7513
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-17 02:20:37 +0500

    Solved 1528 - Shuffle string

---
+++ Leetcode/1528_ShuffleString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System;
+
+namespace _1528_ShuffleString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RestoreString("codeleet", new int[] { 4, 5, 6, 7, 0, 2, 1, 3 }));
+        }
+
+        public static string RestoreString(string s, int[] indices)
+        {
+            StringBuilder res = new StringBuilder(s);
+            for (int i = 0; i < indices.Length; i++)
+            {
+                res[indices[i]] = s[i];
+            }
+
+            return res.ToString();
+        }
+    }
+}
