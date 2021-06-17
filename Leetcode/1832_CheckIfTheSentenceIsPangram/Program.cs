e86e062a5ddec58173282941abd45491d1636507
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-18 00:14:38 +0500

    Solved 1832-Check if the sentence is pangram

---
+++ Leetcode/1832_CheckIfTheSentenceIsPangram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1832_CheckIfTheSentenceIsPangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+            Console.WriteLine(CheckIfPangram("leetcode"));
+        }
+        public static bool CheckIfPangram(string sentence)
+        {
+            int[] t = new int[26];
+            foreach (var i in sentence)
+            {
+                int index = (int)i - 97;
+                t[index]++;
+            }
+
+
+            bool res = true;
+            for (int i = 0; i < 26; i++)
+            {
+                if (t[i] == 0)
+                {
+                    res = false;
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
