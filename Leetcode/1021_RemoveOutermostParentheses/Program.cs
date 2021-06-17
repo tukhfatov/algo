a90e53781ae9c3181a40bb2bc64a4c9265f03e46
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-17 23:55:09 +0500

    Update solution of 1021 Remove outermost parentheses

--- Leetcode/1021_RemoveOutermostParentheses/Program.cs
+++ Leetcode/1021_RemoveOutermostParentheses/Program.cs
@@ -1,32 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace _1021_RemoveOutermostParentheses {
-    class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
+namespace _1021_RemoveOutermostParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
         }
 
-        public static string RemoveOuterParentheses (string S) {
+        public static string RemoveOuterParentheses(string S)
+        {
             bool[] f = new bool[S.Length];
-            Stack<char> st = new Stack<char> ();
+            Stack<char> st = new Stack<char>();
 
-            for (int i = 0; i < S.Length; i++) {
-                if (S[i] == '(') {
-                    st.Push ('(');
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '(')
+                {
+                    st.Push('(');
                 }
                 f[i] = (st.Count == 1);
-                if (S[i] == ')') {
-                    st.Pop ();
+                if (S[i] == ')')
+                {
+                    st.Pop();
                 }
             }
-            StringBuilder res = new StringBuilder ();
-            for (int i = 0; i < S.Length; i++) {
-                if (!f[i]) {
-                    res.Append (S[i]);
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (!f[i])
+                {
+                    res.Append(S[i]);
                 }
             }
 
-            return res.ToString ();
+            return res.ToString();
 
         }
     }
