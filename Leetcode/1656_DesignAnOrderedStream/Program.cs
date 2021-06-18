de906085432d581bfaaae48372eac09ec6a36cf7
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-19 03:03:30 +0500

    Solved 1656 - Design an Ordered stream

---
+++ Leetcode/1656_DesignAnOrderedStream/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace _1656_DesignAnOrderedStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+    public class OrderedStream
+    {
+
+        private int cur;
+        private int total;
+        private Dictionary<int, string> dict;
+
+
+        public OrderedStream(int n)
+        {
+            dict = new Dictionary<int, string>(n);
+            total = n;
+            cur = 1;
+        }
+
+        public IList<string> Insert(int idKey, string value)
+        {
+            dict.Add(idKey, value);
+            var res = new List<string>();
+            for (int i = cur; i <= total; i++)
+            {
+                if (dict.ContainsKey(i))
+                {
+                    cur = i + 1;
+                    res.Add(dict[i]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+
+}
