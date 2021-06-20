38090a309b2aff22e046d0da0873b74961aee66b
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-21 03:26:17 +0500

    Solved 1688 - Count of match in tournament

---
+++ Leetcode/1688_CountOfMatchInTournament/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1688_CountOfMatchInTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfMatches(7));
+            Console.WriteLine(NumberOfMatches(14));
+        }
+
+        public static int NumberOfMatches(int n)
+        {
+            int res = 0;
+
+            while (n > 1)
+            {
+                if (n % 2 == 1)
+                {
+                    res = res + (n - 1) / 2;
+                    n = ((n - 1) / 2) + 1;
+                }
+                else
+                {
+                    res = res + n / 2;
+                    n = n / 2;
+                }
+            }
+
+            return res;
+
+        }
+    }
+}
