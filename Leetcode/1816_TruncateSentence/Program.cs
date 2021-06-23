c35585c3bffe9268a18c511560b67e8791ab9511
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-23 23:41:02 +0500

    Solved 1816-Truncate sentence

---
+++ Leetcode/1816_TruncateSentence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1816_TruncateSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TruncateSentence("Hello how are you Contestant", 4));
+        }
+
+        public static string TruncateSentence(string s, int k)
+        {
+            var a = s.Split(' ');
+            StringBuilder r = new StringBuilder(k);
+            for (int i = 0; i < k; i++)
+            {
+                r.Append(a[i] + " ");
+            }
+
+            return r.ToString().Trim();
+        }
+    }
+}
