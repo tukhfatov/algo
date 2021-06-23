bab0bfa2c9ef7cf25ac1a4fe762ba148c36e3e7d
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-24 00:53:01 +0500

    Solved 1732 - Fint the Highest Altitude

---
+++ Leetcode/1732_FindTheHighestAltitude/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1732_FindTheHighestAltitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LargestAltitude(new int[] { -5, 1, 5, 0, -7 }));
+            Console.WriteLine(LargestAltitude(new int[] { -4, -3, -2, -1, 4, 3, 2 }));
+        }
+        public static int LargestAltitude(int[] gain)
+        {
+            int res = 0;
+            int tmp = 0;
+            foreach (int i in gain)
+            {
+                tmp = tmp + i;
+                if (tmp > res)
+                {
+                    res = tmp;
+                }
+            }
+
+            return res;
+        }
+    }
+}
