de74d65098b67e10ca44228ee4a2d42cf067b5cc
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-25 02:33:53 +0500

    Solved 1725-Number of rectangles that can form the largest square

---
+++ Leetcode/1725_NumberOfRectanglesThatCanFormTheLargestSquare/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace _1725_NumberOfRectanglesThatCanFormTheLargestSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountGoodRectangles(new int[][] {
+                new int[] {5,8},
+                new int[] {3,9},
+                new int[] {5,12},
+                new int[] {16,5},
+            }));
+
+            Console.WriteLine(CountGoodRectangles(new int[][] {
+                new int[] {2,3},
+                new int[] {3,7},
+                new int[] {4,3},
+                new int[] {3,7},
+            }));
+        }
+
+        public static int CountGoodRectangles(int[][] rectangles)
+        {
+            int count = 0, mx = 0;
+            for (int i = 0; i < rectangles.Length; i++)
+            {
+                int m = Math.Min(rectangles[i][0], rectangles[i][1]);
+                if (m > mx)
+                {
+                    mx = m;
+                    count = 1;
+                }
+
+                else if (m == mx)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
