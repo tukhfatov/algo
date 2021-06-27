83cc7acd10aadff002fb36e8e37575950a551a4f
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-27 23:12:33 +0500

    Solved [1913] in O(n)

--- Leetcode/1913_MaximumProductDifferenceBetweenTwoPairs/Program.cs
+++ Leetcode/1913_MaximumProductDifferenceBetweenTwoPairs/Program.cs
@@ -7,13 +7,48 @@ namespace _1913_MaximumProductDifferenceBetweenTwoPairs
         static void Main(string[] args)
         {
             Console.WriteLine(MaxProductDifference(new int[] { 5, 6, 2, 7, 4 }));
-            Console.WriteLine(MaxProductDifference(new int[] { 4, 2, 5, 9, 7, 4, 8 }));
+            Console.WriteLine(MaxProductDifferenceFaster(new int[] { 4, 2, 5, 9, 7, 4, 8 }));
         }
-
+        //  Solution in O(nlogn)
         public static int MaxProductDifference(int[] nums)
         {
             Array.Sort(nums);
             return (nums[nums.Length - 2] * nums[nums.Length - 1]) - (nums[0] * nums[1]);
         }
+
+        // Solution in O(n)
+        public static int MaxProductDifferenceFaster(int[] nums)
+        {
+            int max1 = 0;
+            int max2 = 0;
+            int min1 = Int32.MaxValue;
+            int min2 = Int32.MaxValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > max1)
+                {
+                    max2 = max1;
+                    max1 = nums[i];
+                }
+                else if (nums[i] > max2)
+                {
+                    max2 = nums[i];
+                }
+
+                if (nums[i] < min1)
+                {
+                    min2 = min1;
+                    min1 = nums[i];
+                }
+                else if (nums[i] < min2)
+                {
+                    min2 = nums[i];
+                }
+            }
+
+            return (max1 * max2) - (min1 * min2);
+        }
+
     }
 }
