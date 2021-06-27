ce708c61ce89af479c7652c3a2f693e4bf174752
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-27 22:43:47 +0500

    Solved 1572-Max diagonal sum

---
+++ Leetcode/1572_MatrixDiagonalSum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _1572_MatrixDiagonalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                DiagonalSum(new int[][]{
+                    new int[]{1,2,3},
+                    new int[]{4,5,6},
+                    new int[]{7,8,9}
+                })
+            );
+
+            Console.WriteLine(
+                DiagonalSum(new int[][]{
+                    new int[]{1,1,1,1},
+                    new int[]{1,1,1,1},
+                    new int[]{1,1,1,1},
+                    new int[]{1,1,1,1}
+                })
+            );
+
+            Console.WriteLine(
+                DiagonalSum(new int[][]{
+                    new int[]{5},
+                })
+            );
+
+        }
+
+        public static int DiagonalSum(int[][] mat)
+        {
+            int res = 0;
+
+            for (int i = 0; i < mat.Length; i++)
+            {
+                res += mat[i][i];
+            }
+
+            int j = mat.Length - 1;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                res += mat[i][j--];
+            }
+
+            if (mat.Length % 2 == 1)
+            {
+                res -= mat[mat.Length / 2][mat.Length / 2];
+            }
+            return res;
+        }
+    }
+}
