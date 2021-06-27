cd59a75f6a87b26a81dfd515c4d11ed5ceda373b
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-06-27 23:02:26 +0500

    Solved [1913] Maximum product diff between two pairs

---
+++ Leetcode/1913_MaximumProductDifferenceBetweenTwoPairs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1913_MaximumProductDifferenceBetweenTwoPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxProductDifference(new int[] { 5, 6, 2, 7, 4 }));
+            Console.WriteLine(MaxProductDifference(new int[] { 4, 2, 5, 9, 7, 4, 8 }));
+        }
+
+        public static int MaxProductDifference(int[] nums)
+        {
+            Array.Sort(nums);
+            return (nums[nums.Length - 2] * nums[nums.Length - 1]) - (nums[0] * nums[1]);
+        }
+    }
+}
