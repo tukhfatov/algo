789111ef61970a09b4bc74f1d9c92d3537b6fb0b
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-02 02:01:54 +0500

    Solved [1812] Determine color of a chessboard square

---
+++ Leetcode/1812_DetermineColorOfAChessboardSquare/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1812_DetermineColorOfAChessboardSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SquareIsWhite("a1"));
+            Console.WriteLine(SquareIsWhite("h3"));
+            Console.WriteLine(SquareIsWhite("c7"));
+        }
+
+        public static bool SquareIsWhite(string coordinates)
+        {
+            string cols = "abcdefgh";
+
+            int col = cols.IndexOf(coordinates[0]) + 1;
+            int row = Convert.ToInt32(coordinates[1]);
+
+            if ((col + row) % 2 == 0)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+    }
+}
