16e9fa719bc08b4b09338725401ae2459e3d3009
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-02 23:45:34 +0500

    Solved [1837] Sum of digits in base K

---
+++ Leetcode/1837_SumOfDigitsInBaseK/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1837_SumOfDigitsInBaseK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumBase(34, 6));
+            Console.WriteLine(SumBase(10, 10));
+        }
+
+        public static int SumBase(int n, int k)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int p = n % k;
+                sum += p;
+                n = n / k;
+            }
+
+            return sum;
+        }
+    }
+}
