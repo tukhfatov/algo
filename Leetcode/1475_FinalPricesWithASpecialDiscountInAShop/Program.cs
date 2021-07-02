4ebce8be026471ba2d7499d8a09454d8ba74ceba
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-02 23:58:52 +0500

    Solved [1475] Final prices with a special discount in a shop

---
+++ Leetcode/1475_FinalPricesWithASpecialDiscountInAShop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1475_FinalPricesWithASpecialDiscountInAShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(" ", FinalPrices(new int[] { 8, 4, 6, 2, 3 })));
+            Console.WriteLine(String.Join(" ", FinalPrices(new int[] { 1, 2, 3, 4, 5 })));
+            Console.WriteLine(String.Join(" ", FinalPrices(new int[] { 10, 1, 1, 6 })));
+        }
+
+        public static int[] FinalPrices(int[] prices)
+        {
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    if (prices[j] <= prices[i])
+                    {
+                        prices[i] = prices[i] - prices[j];
+                        break;
+                    }
+                }
+            }
+
+            return prices;
+        }
+    }
+}
