6ac0e214e82d7edcc293d5e6adf174c4bd6ffdd0
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-03 00:39:14 +0500

    Solved [1748] Sum of unique elements

---
+++ Leetcode/1748_SumOfUniqueElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace _1748_SumOfUniqueElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfUnique(new int[] { 1, 2, 3, 2 }));
+        }
+
+        public static int SumOfUnique(int[] nums)
+        {
+            Dictionary<int, int> a = new Dictionary<int, int>();
+
+            foreach (var i in nums)
+            {
+                if (a.ContainsKey(i))
+                {
+                    a[i]++;
+                }
+                else
+                {
+                    a[i] = 1;
+                }
+            }
+
+            int res = 0;
+            foreach (var (k, v) in a)
+            {
+                if (v == 1)
+                {
+                    res += k;
+                }
+            }
+
+            return res;
+        }
+    }
+}
