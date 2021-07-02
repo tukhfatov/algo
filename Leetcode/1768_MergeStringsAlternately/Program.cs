7a1e2750cf49b06d19e62fb6728fcdd79d36cffb
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-03 01:20:51 +0500

    Solved [1768] Merge strings alternately

---
+++ Leetcode/1768_MergeStringsAlternately/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System;
+
+namespace _1768_MergeStringsAlternately
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MergeAlternately("abc", "pqr"));
+            Console.WriteLine(MergeAlternately("ab", "pqrs"));
+            Console.WriteLine(MergeAlternately("abcd", "pq"));
+        }
+
+        public static string MergeAlternately(string word1, string word2)
+        {
+            int n = word1.Length >= word2.Length ? word1.Length : word2.Length;
+
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                if (i < word1.Length)
+                {
+                    s.Append(word1[i]);
+                }
+
+                if (i < word2.Length)
+                {
+                    s.Append(word2[i]);
+                }
+            }
+
+            return s.ToString();
+        }
+
+    }
+}
