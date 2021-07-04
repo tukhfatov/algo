2b29b059bb292b32ab3d558e095773578cdee9ee
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-04 18:55:22 +0500

    Solved [1920] Build array from permutation

---
+++ Leetcode/1920_BuildArrayFromPermutation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1920_BuildArrayFromPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(" ", BuildArray(new int[] { 0, 2, 1, 5, 3, 4 })));
+            Console.WriteLine(String.Join(" ", BuildArray(new int[] { 5, 0, 1, 2, 3, 4 })));
+        }
+
+        public static int[] BuildArray(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res[i] = nums[nums[i]];
+            }
+
+            return res;
+        }
+    }
+}
