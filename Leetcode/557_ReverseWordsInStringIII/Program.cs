1c07c15beacc4f09e24bedb4e1170bc64d6622ee
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-11 23:58:27 +0500

    Solved [557] Reverse words in string III

---
+++ Leetcode/557_ReverseWordsInStringIII/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System;
+
+namespace _557_ReverseWordsInStringIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("Let's take LeetCode contest"));
+            Console.WriteLine(ReverseWords("God Ding"));
+
+        }
+
+        public static string ReverseWords(string s)
+        {
+            string[] words = s.Split();
+
+            StringBuilder res = new StringBuilder(s.Length);
+
+            foreach (var word in words)
+            {
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    res.Append(word[i]);
+                }
+                res.Append(" ");
+            }
+
+            return res.ToString().Trim();
+        }
+    }
+}
