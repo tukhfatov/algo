bca8cf2f862e67b5b331c3455d69504c8eefcd69
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-21 01:40:04 +0500

    Solved [1935] Maximum number of words you can type

---
+++ Leetcode/1935_MaximumNumberOfWordsYouCanType/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1935_MaximumNumberOfWordsYouCanType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanBeTypedWords("hello world", "ad"));
+            Console.WriteLine(CanBeTypedWords("leet code", "lt"));
+            Console.WriteLine(CanBeTypedWords("leet code", "e"));
+        }
+
+        public static int CanBeTypedWords(string text, string brokenLetters)
+        {
+            int count = 0;
+            var words = text.Split();
+
+            foreach (var word in words)
+            {
+                count++;
+                foreach (var l in word)
+                {
+                    if (brokenLetters.Contains(l))
+                    {
+                        count--;
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
