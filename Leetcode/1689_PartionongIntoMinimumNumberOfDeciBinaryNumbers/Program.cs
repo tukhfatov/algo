b3abcb145ca2c9e8c736419cb97cdebabd831ded
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-07-28 22:23:20 +0500

    Solved [1689] Paritoning into minimum number of deci binary numbers

---
+++ Leetcode/1689_PartionongIntoMinimumNumberOfDeciBinaryNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1689_PartionongIntoMinimumNumberOfDeciBinaryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinPartitions("32"));
+            Console.WriteLine(MinPartitions("82734"));
+            Console.WriteLine(MinPartitions("27346209830709182346"));
+        }
+        public static int MinPartitions(string n)
+        {
+            int m = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (Convert.ToInt32(n[i] - '0') > m)
+                {
+                    m = Convert.ToInt32(n[i] - '0');
+                }
+            }
+
+            return m;
+        }
+    }
+}
