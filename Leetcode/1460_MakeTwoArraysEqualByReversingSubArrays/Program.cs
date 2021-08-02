29379e97cd0d36cfdf8637bd8cdac4ba6072aeff
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-08-02 23:03:39 +0500

    Solved [1460] Make two arrays equal by reversing sub arrays

---
+++ Leetcode/1460_MakeTwoArraysEqualByReversingSubArrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _1460_MakeTwoArraysEqualByReversingSubArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanBeEqual(new int[] { 1, 2, 3, 4 }, new int[] { 2, 4, 1, 3 }));
+            Console.WriteLine(CanBeEqual(new int[] { 7 }, new int[] { 7 }));
+            Console.WriteLine(CanBeEqual(new int[] { 1, 12 }, new int[] { 12, 1 }));
+            Console.WriteLine(CanBeEqual(new int[] { 3, 7, 9 }, new int[] { 3, 7, 11 }));
+        }
+
+        public static bool CanBeEqual(int[] target, int[] arr)
+        {
+            if (target.Length != arr.Length)
+            {
+                return false;
+            }
+
+            Array.Sort(target);
+            Array.Sort(arr);
+
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                if (target[i] != arr[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
