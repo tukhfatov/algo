01b5d5a3b0ae96df52db4c01f0de29df22f7f492
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-08-15 01:49:04 +0500

    Solved [344] Reverse string

---
+++ Leetcode/344_ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _344_ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseString(new char[] { 'h', 'e', 'l', 'l', 'o' });
+            ReverseString(new char[] { 'H', 'a', 'n', 'n', 'a', 'h' });
+        }
+
+        public static void ReverseString(char[] s)
+        {
+
+            int j = s.Length - 1;
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                char tmp = s[i];
+                s[i] = s[j];
+                s[j] = tmp;
+                j--;
+            }
+
+            foreach (var i in s)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
