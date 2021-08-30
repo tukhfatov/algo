c1b7d0294ea470fd22e134137513573eb0c7f197
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-08-31 01:00:16 +0500

    Solved [1967] Numer of strings that appear as substrings in word

---
+++ Leetcode/1967_NumberOfStringsThatAppearAsSubstringsInWord/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1967_NumberOfStringsThatAppearAsSubstringsInWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumOfStrings(new string[] { "a", "abc", "bc", "d" }, "abc"));
+            Console.WriteLine(NumOfStrings(new string[] { "a", "b", "c" }, "aaaaabbbbb"));
+            Console.WriteLine(NumOfStrings(new string[] { "a", "a", "a" }, "ab"));
+        }
+
+        public static int NumOfStrings(string[] patterns, string word)
+        {
+            int count = 0;
+
+            foreach (string pattern in patterns)
+            {
+                if (word.Contains(pattern))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
