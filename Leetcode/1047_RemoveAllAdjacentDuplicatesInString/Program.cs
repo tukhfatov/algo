4c90ef879ab2d71208357baf61a1b70e62160dd7
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-04 01:31:09 +0500

    Solved [1047] Remove all adjacent duplicates in string

---
+++ Leetcode/1047_RemoveAllAdjacentDuplicatesInString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace _1047_RemoveAllAdjacentDuplicatesInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveDuplicates("abbaca"));
+            Console.WriteLine(RemoveDuplicates("azxxzy"));
+        }
+
+        public static string RemoveDuplicates(string s)
+        {
+            Stack<char> st = new Stack<char>();
+
+            foreach (char l in s)
+            {
+                if (st.Count > 0 && st.Peek() == l)
+                {
+                    st.Pop();
+                }
+                else
+                {
+                    st.Push(l);
+                }
+            }
+            string res = "";
+            foreach (char l in st)
+            {
+                res = l + res;
+            }
+            return res;
+        }
+    }
+}
