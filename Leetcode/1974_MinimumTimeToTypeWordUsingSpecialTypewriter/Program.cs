9fd9b838fd785e3550841bbb82d6adc0d5a942f5
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-04 00:45:35 +0500

    Solved [1974] Minimum time to type word using special typewriter

---
+++ Leetcode/1974_MinimumTimeToTypeWordUsingSpecialTypewriter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1974_MinimumTimeToTypeWordUsingSpecialTypewriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinTimeToType("abc"));
+            Console.WriteLine(MinTimeToType("bza"));
+            Console.WriteLine(MinTimeToType("zjpc"));
+
+        }
+
+        public static int MinTimeToType(string word)
+        {
+            int s = 0;
+            int cur = 'a';
+
+            foreach (char l in word)
+            {
+                int cc = Math.Abs(cur - l);
+                if (cc > 13)
+                {
+                    cc = 26 - cc;
+                }
+                s += cc + 1;
+                cur = l;
+            }
+
+            return s;
+        }
+    }
+}
