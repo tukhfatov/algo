cd901783479f74a7b8375c047aa44424f51c0e2d
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-04 01:50:28 +0500

    Solved [704] Binary search

---
+++ Leetcode/704_BinarySearch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _704_BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Search(new int[] { -1, 0, 3, 5, 9, 12 }, 9));
+            Console.WriteLine(Search(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+            int i = -1;
+            int l = 0;
+            int r = nums.Length;
+
+
+            while (l != r)
+            {
+                int m = (l + r) / 2;
+                if (nums[m] == target)
+                {
+                    i = m;
+                    break;
+                }
+                if (nums[m] > target)
+                {
+                    r = m;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+
+            return i;
+        }
+    }
+}
