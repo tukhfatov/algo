6cbfcfd17eef32104d5d103c073885b3de61d6f1
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-13 02:40:19 +0500

    Solved [2000] Reverse prefix of word

---
+++ Leetcode/2000_ReversePrefixOfWord/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2000_ReversePrefixOfWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReversePrefix("abcdefd", 'd'));
+            Console.WriteLine(ReversePrefix("xyxzxe", 'z'));
+            Console.WriteLine(ReversePrefix("abcd", 'z'));
+        }
+
+        public static string ReversePrefix(string word, char ch)
+        {
+            int ind = word.IndexOf(ch);
+            if (ind > 0)
+            {
+                string tmp = "";
+                string sec = word.Substring(ind + 1);
+                while (ind >= 0)
+                {
+                    tmp += word[ind];
+                    ind--;
+                }
+                tmp = tmp + sec;
+                return tmp;
+            }
+
+            return word;
+        }
+    }
+}
