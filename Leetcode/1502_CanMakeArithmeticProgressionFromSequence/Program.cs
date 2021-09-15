d85fbc8e60ffbfd2b4e71c6c5ecdd783290e0b63
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-15 23:20:45 +0500

    Solved [1502] Can make arithmetic progression from sequence

---
+++ Leetcode/1502_CanMakeArithmeticProgressionFromSequence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1502_CanMakeArithmeticProgressionFromSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanMakeArithmeticProgression(new int[] { 3, 5, 1 }));
+        }
+
+        public static bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+            int dif = Math.Abs(arr[0] - arr[1]);
+
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (Math.Abs(arr[i] - arr[i + 1]) != dif)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
