52874f00fb529ec406f570dc6926df614cd672f6
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-15 23:12:12 +0500

    Solved [944] Delete columns to make sorted

---
+++ Leetcode/944_DeleteColumnsToMakeSorted/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _944_DeleteColumnsToMakeSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinDeletionSize(new string[] {
+                "cba","daf","ghi"
+            }));
+            Console.WriteLine(MinDeletionSize(new string[] {
+                "a", "b"
+            }));
+            Console.WriteLine(MinDeletionSize(new string[] {
+                "zyx","wvu","tsr"
+            }));
+
+            Console.WriteLine(MinDeletionSize(new string[] {
+                "rrjk","furt","guzm"
+            }));
+        }
+
+        public static int MinDeletionSize(string[] strs)
+        {
+            int res = 0;
+            int cn = strs[0].Length;
+            for (int j = 0; j < cn; j++)
+            {
+                for (int i = 0; i < strs.Length - 1; i++)
+                {
+                    if (strs[i][j] > strs[i + 1][j])
+                    {
+                        res++;
+                        break;
+                    }
+
+                }
+            }
+
+            return res;
+        }
+    }
+}
