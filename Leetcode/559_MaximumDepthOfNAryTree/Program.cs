7307e481c7e393550066d6d408f8bbcdca146249
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-18 22:54:24 +0500

    Solved [559] Maximum depath of n-ary tree

---
+++ Leetcode/559_MaximumDepthOfNAryTree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _559_MaximumDepthOfNAryTree
+{
+    class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxDepth(new Node(1, new List<Node>
+            {
+                new Node(3, new List<Node>{
+                    new Node(5, new List<Node>()),
+                    new Node(6, new List<Node>())
+                }),
+                new Node(2, new List<Node>()),
+                new Node(4, new List<Node>())
+            })));
+        }
+
+        public static int MaxDepth(Node root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int max = 1;
+            foreach (var i in root.children)
+            {
+                max = Math.Max(max, MaxDepth(i) + 1);
+            }
+
+            return max;
+        }
+    }
+}
