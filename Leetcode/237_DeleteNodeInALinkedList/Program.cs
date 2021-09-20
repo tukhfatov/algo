12c0384085b277f9cac76402081b4447eec2fed4
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-21 02:08:28 +0500

    Solved [237] Delete node in a linked list

---
+++ Leetcode/237_DeleteNodeInALinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _237_DeleteNodeInALinkedList
+{
+    class Program
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        static void Main(string[] args)
+        {
+            ListNode l = new ListNode(4);
+
+            Console.WriteLine(DeleteNode(l));
+        }
+
+        public static void DeleteNode(ListNode node)
+        {
+            while (node != null)
+            {
+                node.val = node.next.val;
+                if (node.next.next == null)
+                {
+                    node.next = null;
+                }
+
+                node = node.next;
+            }
+
+        }
+    }
+}
