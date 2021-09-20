39a6a83a7dd7e6b27ada047e1cd495f1dab9e13c
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-21 03:24:43 +0500

    Solved [883] Projection area of 3D Shapes

---
+++ Leetcode/883_ProjectionAreaOf3DShapes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _883_ProjectionAreaOf3DShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ProjectionArea(new int[][]{
+                new int[2]
+            }));
+        }
+        public static int ProjectionArea(int[][] grid)
+        {
+            int total = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                int c = 0;
+                int cm = Int32.MinValue;
+                int rm = Int32.MinValue;
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    rm = Math.Max(rm, grid[i][j]);
+                    cm = Math.Max(cm, grid[j][i]);
+                    if (grid[i][j] > 0)
+                    {
+                        c++;
+                    }
+                }
+                total += rm;
+                total += cm;
+                total += c;
+            }
+
+            return total;
+        }
+    }
+}
