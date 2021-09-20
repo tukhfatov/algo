91f3813862f84d26576d732f859e1a7965159344
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-21 03:30:30 +0500

    Solved [226] Invert binary tree

---
+++ Leetcode/226_InvertBinaryTree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _226_InvertBinaryTree
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(InvertTree(new TreeNode
+            {
+                val = 2,
+                left = new TreeNode
+                {
+                    val = 3
+                },
+                right = new TreeNode
+                {
+                    val = 1
+                }
+            }));
+        }
+
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            TreeNode tmp = root.left;
+            root.left = root.right;
+            root.right = tmp;
+
+            return root;
+        }
+    }
+}
