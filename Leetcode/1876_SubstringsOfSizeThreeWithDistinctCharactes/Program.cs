40b0a48971b4760801e3cbf268d9c774c6b383aa
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2021-09-21 22:24:26 +0500

    Solved [1876] Substrings of size three with distinct charactes

---
+++ Leetcode/1876_SubstringsOfSizeThreeWithDistinctCharactes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1876_SubstringsOfSizeThreeWithDistinctCharactes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountGoodSubstrings("xyzzaz"));
+            Console.WriteLine(CountGoodSubstrings("aababcabc"));
+        }
+
+        public static int CountGoodSubstrings(string s)
+        {
+            int total = 0;
+            for (int i = 1; i < s.Length - 1; i++)
+            {
+                if (s[i] != s[i - 1] && s[i - 1] != s[i + 1] && s[i] != s[i + 1])
+                {
+                    total++;
+                }
+            }
+
+            return total;
+        }
+    }
+}
